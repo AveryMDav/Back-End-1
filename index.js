b46c7cd03c91a530890079b2796158e3ea1949d8
Avery.Davenport <averymdavenport@gmail.com>
2021-06-14 15:11:13 -0600

    3-commit

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/users', function(req, res){
-    const myArr = ['Cody', 'Thomas', 'Bree', 'Ryan', 'Chris', 'Lindsey'];
-    res.status(200).send(myArr);
+app.get('/api/users', function(req,res){
+    const friends = ['cody','thomas','bree','ryan','chris'];
+    res.status(200).send(friends);
 });
 
-app.get('/weather/:temp', function(req, res){
-    const {temp} = req.params;
-    const phrase = `<h3>It was ${temp} today.<h3>`;
+
+app.get('/weather/:temperature', function(req,res){
+    const {temperature} = req.params;
+    const phrase = `<h3>The weather was ${temperature} today.</h3>`;
     res.status(200).send(phrase);
 });
 
-const SERVER_PORT = 4000;
-app.listen(SERVER_PORT, function(){console.log(`Server is up on ${SERVER_PORT}`)})
+
+app.listen(4000, function(){console.log('Server is up on 4000')});
+
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get('/api/users', function(req, res){
+//     const myArr = ['Cody', 'Thomas', 'Bree', 'Ryan', 'Chris', 'Lindsey'];
+//     res.status(200).send(myArr);
+// });
+
+// app.get('/weather/:temp', function(req, res){
+//     const {temp} = req.params;
+//     const phrase = `<h3>It was ${temp} today.<h3>`;
+//     res.status(200).send(phrase);
+// });
+
+// const SERVER_PORT = 4000;
+// app.listen(SERVER_PORT, function(){console.log(`Server is up on ${SERVER_PORT}`)})
